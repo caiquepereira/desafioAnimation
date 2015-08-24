08318a86585257de826e696f0b583fdff3fc3594
caiquepereira <caiquepereira@gmail.com>
2015-08-24 16:32:30 -0300

    Adicionado o reconhecimento de gestos

--- DesafioTangram/DesafioTagram/DesafioTagram/ViewController.swift
+++ DesafioTangram/DesafioTagram/DesafioTagram/ViewController.swift
@@ -18,7 +18,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
         
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let tap = UITapGestureRecognizer(target: self, action: Selector("oneTap"))
+        tap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: Selector("doubleTap"))
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap)
+        
+        let tripleTap = UITapGestureRecognizer(target: self, action: Selector("tripleTap"))
+        tripleTap.numberOfTapsRequired = 3
+        self.view.addGestureRecognizer(tripleTap)
+        
+        let twoFingers = UITapGestureRecognizer(target: self, action: Selector("twoFingers"))
+        twoFingers.numberOfTouchesRequired = 2
+        self.view.addGestureRecognizer(twoFingers)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: "longPressed:")
+        self.view.addGestureRecognizer(longPressRecognizer)
         
         
         
@@ -33,6 +66,68 @@ class ViewController: UIViewController {
         
     }
 
+    
+    func oneTap() {
+        
+        println("one tap")
+        
+        
+    }
+    
+    func doubleTap() {
+        
+        println("double tap")
+        
+        
+    }
+    
+    func tripleTap() {
+        
+        println("triple tap")
+        
+        
+    }
+    
+    func twoFingers() {
+        
+        println("2 fingers")
+        
+        
+    }
+    
+    func longPressed(sender: UILongPressGestureRecognizer)
+    {
+        println("longpressed")
+    }
+    
+    
+    
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Up:
+                println("Swiped up")
+            case UISwipeGestureRecognizerDirection.Down:
+                println("Swiped down")
+            case UISwipeGestureRecognizerDirection.Left:
+                println("Swiped left")
+            case UISwipeGestureRecognizerDirection.Right:
+                println("Swiped right")
+            
+            default:
+                break
+            }
+        }
+    }
+    
+    
+
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
